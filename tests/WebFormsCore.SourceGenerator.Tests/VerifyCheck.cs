3ae27f9afb397f5fd3f8530ced67ed3cfe295595
Gerard Smit <gsmit1996@gmail.com>
2024-09-06 16:22:29 +0200

    Giving up

---
+++ tests/WebFormsCore.SourceGenerator.Tests/VerifyCheck.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using VerifyXunit;
+
+namespace WebFormsCore.SourceGenerator.Tests;
+
+public class VerifyChecksTests
+{
+    [Fact]
+    public Task Run() => VerifyChecks.Run();
+}
---
+++ tests/WebFormsCore.Tests/VerifyCheck.cs
@@ -0,0 +1,7 @@
+﻿namespace WebFormsCore.Tests;
+
+public class VerifyChecksTests
+{
+    [Fact]
+    public Task Run() => VerifyChecks.Run();
+}
